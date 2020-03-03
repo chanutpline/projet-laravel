68fd3defefbdf83505161fe6044d98ac69913815
pauline <pauline@pauline-ThinkPad>
2020-03-03 13:45:10 +0100

    début

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    //
+
+    function index()
+    {
+        return view('welcome');
+    }
+
+
+    function contact()
+    {
+        return view('contact');
+    }
+}
