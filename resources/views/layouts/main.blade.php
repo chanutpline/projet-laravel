d8c77e86293877c29e89b08faa7f1085e49d238f
Fatima NASSER <f.nasser.barakat@gmail.com>
2020-04-07 23:25:03 +0200

    identification et authentification

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,8 +5,11 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <title>Projet PHP</title>
 <link rel="stylesheet" href="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.min.css">
+@include('layouts/header')
 </head>
 <body>
+
+
 <div class="top-bar">
 <div class="top-bar-left">
 <ul class="menu">
